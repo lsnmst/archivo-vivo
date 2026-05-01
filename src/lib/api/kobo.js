2f3981bd687d83754b70c93788b524856fba0b99
lsnmst <musetta.alessandro@gmail.com>
2026-05-01 09:59:58 +0200

    fetchKobo function

--- src/lib/api/kobo.js
+++ src/lib/api/kobo.js
@@ -1,16 +1,9 @@
 import { parseItem } from "../utils/parseKobo";
 
-const KOBO_TOKEN = import.meta.env.VITE_KOBO_TOKEN;
-
-const KOBO_URL =
-    "/kobo/api/v2/assets/a5iWLemmhk3UXJjctWsLgr/data/?format=json&attachments=true";
+const KOBO_URL = "https://archivo-vivo-kobo.musetta-alessandro.workers.dev/";
 
 export async function fetchKobo() {
-  const res = await fetch(KOBO_URL, {
-    headers: {
-      Authorization: `Token ${import.meta.env.VITE_KOBO_TOKEN}`
-    }
-  });
+  const res = await fetch(KOBO_URL);
 
   if (!res.ok) {
     const text = await res.text();
